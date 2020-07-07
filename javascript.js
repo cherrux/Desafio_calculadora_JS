89aff460e1b3d8d7852889f05d6225123d4bf82b
cherrux <j.tapiac2@gmail.com>
2020-07-07 17:18:37 -0400

    1

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+
+function agregar_numero(numeros){
+
+let resultado = document.getElementById("resultados").value;
+
+document.getElementById("resultado").innerHTML = resultado+numeros;
+
+}
+
+ 
